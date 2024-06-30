0dcc1b5de701320ae2126d6615ab0178bda2defb
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2024-06-30 12:43:59 +0200

    Przeniesiono klasę do osobnego pliku

--- src/polanieonline/client/panel/ItemsPanel.java
+++ src/polanieonline/client/panel/ItemsPanel.java
@@ -36,9 +36,6 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.text.AbstractDocument;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.DocumentFilter;
 
 import polanieonline.client.console.XMLConsole;
 import polanieonline.common.filter.NumericDocumentFilter;
@@ -526,44 +523,6 @@ public class ItemsPanel extends JPanel {
 	private void updateXMLConsole() {
 		if (xmlConsole != null) {
 			xmlConsole.refreshXML();
-		}
-	}
-
-	private class NumericDocumentFilter extends DocumentFilter {
-		private final int MAX_VALUE = 32767;
-
-		@Override
-		public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
-			String newText = fb.getDocument().getText(0, fb.getDocument().getLength()) + string;
-			if (isNumeric(newText)) {
-				super.insertString(fb, offset, string, attr);
-			}
-		}
-
-		@Override
-		public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
-			String newText = fb.getDocument().getText(0, fb.getDocument().getLength());
-			newText = newText.substring(0, offset) + text + newText.substring(offset + length);
-			if (isNumeric(newText)) {
-				super.replace(fb, offset, length, text, attrs);
-			}
-		}
-
-		@Override
-		public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
-			super.remove(fb, offset, length);
-		}
-
-		private boolean isNumeric(String text) {
-			if (text == null || text.isEmpty()) {
-				return false;
-			}
-			try {
-				int value = Integer.parseInt(text);
-				return value >= 1 && value <= MAX_VALUE;
-			} catch (NumberFormatException e) {
-				return false;
-			}
 		}
 	}
 }
