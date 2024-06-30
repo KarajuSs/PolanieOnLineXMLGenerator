8f2c71fd11ba53ad7e5d450787284ce6d953d161
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2024-06-30 19:26:10 +0200

    Dodano nową klasę Listener do uproszczenia

---
+++ src/polanieonline/common/listener/SimpleDocumentListener.java
@@ -0,0 +1,27 @@
+package polanieonline.common.listener;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class SimpleDocumentListener implements DocumentListener {
+    private final Runnable updateAction;
+
+    public SimpleDocumentListener(Runnable updateAction) {
+        this.updateAction = updateAction;
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        updateAction.run();
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        updateAction.run();
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        updateAction.run();
+    }
+}
