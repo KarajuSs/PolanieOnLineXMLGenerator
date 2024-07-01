7d798b88a7a18ab3f1fca5dae887b3f262d4141e
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2024-07-01 15:16:14 +0200

    Oczyszczono plik ze spacji

--- src/polanieonline/common/listener/SimpleDocumentListener.java
+++ src/polanieonline/common/listener/SimpleDocumentListener.java
@@ -4,24 +4,24 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 public class SimpleDocumentListener implements DocumentListener {
-    private final Runnable updateAction;
+	private final Runnable updateAction;
 
-    public SimpleDocumentListener(Runnable updateAction) {
-        this.updateAction = updateAction;
-    }
+	public SimpleDocumentListener(Runnable updateAction) {
+		this.updateAction = updateAction;
+	}
 
-    @Override
-    public void insertUpdate(DocumentEvent e) {
-        updateAction.run();
-    }
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		updateAction.run();
+	}
 
-    @Override
-    public void removeUpdate(DocumentEvent e) {
-        updateAction.run();
-    }
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		updateAction.run();
+	}
 
-    @Override
-    public void changedUpdate(DocumentEvent e) {
-        updateAction.run();
-    }
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		updateAction.run();
+	}
 }
