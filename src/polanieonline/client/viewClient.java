e015c80852f7b4ea71a204b32cdc6fe35d68e1c4
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2024-07-02 06:44:25 +0200

    Powiększono wysokość aplikacji

--- src/polanieonline/client/viewClient.java
+++ src/polanieonline/client/viewClient.java
@@ -37,7 +37,7 @@ public class viewClient {
 		JFrame frame = new JFrame("Entities XML struct generator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800, 600);
-		frame.setMinimumSize(new Dimension(800, 600));
+		frame.setMinimumSize(new Dimension(800, 670));
 
 		// Załaduj ikonę
 		try {
